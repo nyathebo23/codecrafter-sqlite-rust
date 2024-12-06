3e6f64cc02446825e3fcc08357304f66965eac02
unknown <franckhebo@gmail.com>
2024-12-06 09:14:05 +0100

    select count

--- src/sql_clause_select/select_count.rs
+++ src/sql_clause_select/select_count.rs
@@ -27,7 +27,7 @@ fn page_data(pages_datas: &mut Vec<u8>, table_name: String, pagesize: u16, cells
         (tbl_name, rootpage) = table_name_and_page(&mut start_area_iter);
         count += 2;
     }
-    if tbl_name == table_name {
+    if tbl_name != table_name {
         let vec: Vec<&u8> = Vec::new();
         return vec;
     }
